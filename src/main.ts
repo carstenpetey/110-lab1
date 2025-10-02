edb2aa4c5878e831d19ad4cd21f58bf73ba14c79
Carsten Petersen <carstenpetersen@Carstens-MacBook-Air.local>
2025-10-02 15:09:22 -0700

    drew lemon in konva

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ const layer = new Konva.Layer();
 stage.add(layer);
 
 // create our shape
-const circle = new Konva.Circle({
+/*const circle = new Konva.Circle({
   x: stage.width() / 2,
   y: stage.height() / 2,
   radius: 70,
@@ -20,9 +20,23 @@ const circle = new Konva.Circle({
   stroke: 'black',
   strokeWidth: 4
 });
+*/
+
+const start_x  = stage.width() / 2;
+const start_y = stage.height() / 2;
+
+const blob = new Konva.Line({
+  points: [start_x, start_y, start_x + 65, start_y + 80, start_x + 130, start_y + 60,
+    start_x + 100, start_y - 30, start_x + 35, start_y - 25, start_x, start_y],
+  stroke: 'black',
+  strokeWidth: 10,
+  fill: '#d7f749ff',
+  closed: true,
+  tension: .6
+})
 
 // add the shape to the layer
-layer.add(circle);
+layer.add(blob);
 
 // add the layer to the stage
 stage.add(layer);
